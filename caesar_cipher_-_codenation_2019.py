a4160c6fcc578a31b4c0a131e79c943f3f4fcd0f
danielfaus <daniel.fsantos@protonmail.com>
2019-08-26 01:02:29 -0300

    changes

--- caesar_cipher_-_codenation_2019.py
+++ caesar_cipher_-_codenation_2019.py
@@ -1,22 +1,21 @@
-__author__ = “Daniel Faustino”
-__copyright__ = “Copyright 2019, Codenation Project 2019”
-__credits__ = [“Daniel Faustino”]
-__license__ = “GPL”
-__version__ = “0.7.8”
-__maintainer__ = “Daniel Faustino”
-__email__ = “daniel.fsantos@protonmail.com”
-__status__ = “dev”
-
+__author__ = "Daniel Faustino"
+__copyright__ = "Copyright 2019, Codenation Project 2019"
+__credits__ = ["Daniel Faustino"]
+__license__ = "GPL"
+__version__ = "0.8.1"
+__maintainer__ = "Daniel Faustino"
+__email__ = "daniel.fsantos@protonmail.com"
+__status__ = "dev"
 
 # import request and post url and token from another file
-from config.py import url_post, url_request, token
+from config import  url_post, url_request, token
 # importing libraries
-import urllib3  # library to HTTP request
-import json      # library to JSON handles
-import hashlib  # library to hash SHA1
+import urllib3                                              # library to HTTP request
+import json                                                 # library to JSON handles
+import hashlib                                              # library to hash SHA1
 
 # starting PoolManages to handles connections
-HTTP = urllib3.poolmanager()
+http = urllib3.PoolManager()
 
 # starting request
 request_get = http.request(
@@ -28,65 +27,65 @@ converting request to JSON
 '''
 # converting to dict type
 request_dict = json.loads(request_get.data)
-print(json.dumps(request_dict, indent=2))   # printing (optional)
-print('\n')                                 # jumping one line (optional)
+print(json.dumps(request_dict, indent=2))                   # printing (optional)
+print('\n')                                                 # jumping one line (optional)
 
 '''
 creating a JSON file
 '''
-answer = open('answer', 'w')    # creating a file
-answer.write(json.dumps(request_get, indent=2))     # writing in file
+answer = open('answer', 'w')                                # creating a file
+answer.write(json.dumps(request_dict, indent=2))             # writing in file
 answer.close()  # closing file
 
 '''
 applying the Caesar Cipher
 '''
-encrypted = request_dict['cifrado']  # get the message
-encrypted = encrypted.lower()  # making message lower case
-key_value = request_dict.get(['numero_casas', default])  # get key
-decrypted = ''  # it's get the descrypted message
+encrypted = request_dict['cifrado']                         # get the message
+encrypted = encrypted.lower()                               # making message lower case
+key_value = request_dict['numero_casas']                    # get key
+decrypted = ''                                              # it's get the decrypted message
 
 # decrypt the message
-for symbol in encrypted:  # for each char in message
-    if symbol.isalpha():  # if char is a letter
-        ascii_code = ord(symbol)  # get the ASCII position
-        ascii_code -= key_value  # get de decrypetd position
+for symbol in encrypted:                                    # for each char in message
+    if symbol.isalpha():                                    # if char is a letter
+        ascii_code = ord(symbol)                            # get the ASCII position
+        ascii_code -= key_value                             # get de decrypetd position
 
-        if ascii_code > ord('z'):  # if letter is out lower alphabet
-            ascii_code -= 26  # positioning in lower alphabet
-        elif ascii_code < ord('a'):  # if letter is out lower alphabet
-            ascii_code += 26  # positioning in lower alphabet
+        if ascii_code > ord('z'):                           # if letter is out lower alphabet
+            ascii_code -= 26                                # positioning in lower alphabet
+        elif ascii_code < ord('a'):                         # if letter is out lower alphabet
+            ascii_code += 26                                # positioning in lower alphabet
 
-        decrypted += chr(ascii_code)  # decrypt each letter
+        decrypted += chr(ascii_code)                        # decrypt each letter
     else:
-        decrypted += symbol  # remain number, point and space
+        decrypted += symbol                                 # remain number, point and space
 
-print('Decrypted Message: ', decrypted)  # print message (optional)
-print('\n')  # jump one line (optional)
+print('Decrypted Message: ', decrypted)                     # print message (optional)
+print('\n')                                                 # jump one line (optional)
 
 '''
 geting SHA1 in decrypted message
 '''
 # get hash
 resume = hashlib.sha1(decrypted.encode()).hexdigest()
-print('HASH: ', resume)  # priting (optional)
-print('\n')  # jump one line (optional)
+print('HASH: ', resume)                                     # priting (optional)
+print('\n')                                                 # jump one line (optional)
 
 # update the value in JSON dict
-request_dict['decifrado'] = decrypted                   # change value
-request_dict['resumo_criptografico'] = resume           # chance value
-answer = open('answer.json', 'w')                       # open file to write
-answer.write(json.dumps(request_dict, indent=2))        # updating the file
-answer.close()                                          # closing the file
+request_dict['decifrado'] = decrypted                       # change value
+request_dict['resumo_criptografico'] = resume               # chance value
+answer = open('answer.json', 'w')                           # open file to write
+answer.write(json.dumps(request_dict, indent=2))            # updating the file
+answer.close()                                              # closing the file
 
-print(json.dumps(request_dict, indent=2))  # printing the updated file
-print('\n')                                             # jumping one line
+print(json.dumps(request_dict, indent=2))                   # printing the updated file
+print('\n')                                                 # jumping one line
 
 # post the file
 with open('answer.json') as answer:
     file_data = answer.read()
 
-req_post = http.request(
+request_post = http.request(
     'POST',
     url_post + token,
     fields={
@@ -94,5 +93,5 @@ req_post = http.request(
     }
 )
 
-print(req_post.status)
-print(json.loads(req_post.data))
+print(request_post.status)
+print(json.loads(request_post.data))
